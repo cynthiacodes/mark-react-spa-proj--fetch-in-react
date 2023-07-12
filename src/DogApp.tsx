22508a785430ec51947b41fd12c5d78cc75c6ac0
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-12 23:47:40 +0100

    testing out css grid layout

--- src/DogApp.tsx
+++ src/DogApp.tsx
@@ -14,24 +14,26 @@ function DogApp() {
   };
 
   return (
-    <>
-      <h1>Dog app</h1>
+    <div className="dogGrid">
+      <h1 className="title">Dog app</h1>
       {dog && (
         <>
-          <img src={dog.message} alt="" />
+          <img className = "image"src={dog.message} alt="" />
+          <hr />
           <button onClick={handleGetPicture}>Get another picture</button>
         </>
       )}
       {!dog && (
         <>
-          <p>
+          <p className="paragraph">
             Click the button to trigger a <code>fetch</code> that gets a random
             dog picture from an API!
           </p>
-          <button onClick={handleGetPicture}>Get picture</button>
+          <hr />
+          <button className = "button"onClick={handleGetPicture}>Get picture</button>
         </>
       )}
-    </>
+    </div>
   );
 
 }
